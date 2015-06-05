2f3515aebcaf247b86d4df46b7bb68eb37e6a1d7
Steven Boyd <steven.boyd@appfolio.com>
2015-06-04 17:46:29 -0700

    sb - add descriptions for the error messages

--- lib/ae_network_connection_exception.rb
+++ lib/ae_network_connection_exception.rb
@@ -26,8 +26,17 @@ module AeNetworkConnectionException
   def self.try
     yield
   rescue SocketError => e
+    # socket error happens when we fail to connect to a socket. Common problems here are DNS resolution (i.e. getaddrinfo)
+    
     raise ConnectionNotEstablished
   rescue SystemCallError => e
+    # Errno::ECONNREFUSED happens when we can not connect to the port
+    # Errno::ETIMEDOUT happens when we timeout durring the tcp handshake
+    # It is important to note, Errno::ETIMEDOUT can also happen after we have established a connection and are waiting for a response.
+    # Because of this, we also check that the sys call that was made is connect(2).
+    # Errno::* Exceptions have the following error message format
+    # "#{Message string} - #{syscall} for "#{host}" port #{port}"
+    
     if ([Errno::ETIMEDOUT, Errno::ECONNREFUSED].include? e.class) && (e.message =~ /connect\(2\)/)
       raise ConnectionNotEstablished
     else
